3184aa860e5ed85d5a66bec98c35cdb3020ad65a
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-09-23 19:24:18 -0300

    Add model Nota

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'views/app.dart';
 
 void main() {
   runApp(NotasApp());
---
+++ lib/models/nota.dart
@@ -0,0 +1,8 @@
+class Nota {
+
+  String id;
+  String title;
+  DateTime date;
+
+  Nota(this.id, this.title, this.date);
+}
