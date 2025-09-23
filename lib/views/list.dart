69d7ada8be4e65fefabe06d9cfdc23280c927596
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-09-23 20:26:05 -0300

    ListTile e lib intl

--- lib/views/list.dart
+++ lib/views/list.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import '../models/nota.dart';
+import 'package:intl/intl.dart';
 
 class ListPage extends StatelessWidget {
+
+  List<Nota> notas = [
+    Nota("1", "Anotações de aula", DateTime.now()),
+    Nota("2", "Anotações de trabalho", DateTime.now()),
+    Nota("3", "Anotações de festas", DateTime.now()),
+    Nota("4", "Anotações de livros", DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +19,24 @@ class ListPage extends StatelessWidget {
         onPressed: () => Navigator.pushNamed(context, "/create"),
         child: Icon(Icons.add),
       ),
+      body: ListView(
+        children: notas.map((nota) => ListTile(
+          title: Text(nota.title),
+          subtitle: Text(DateFormat.yMd('en_US').format(nota.date)),
+          trailing: Icon(Icons.chevron_right_outlined),
+        )).toList()
+      ),
     );
   }
 }
+
+
+// ListTile(
+//             title: Text(
+//               "Anotações de Aula",
+//               maxLines: 1,
+//               overflow: TextOverflow.ellipsis,
+//             ),
+//             subtitle: Text("23/09/2025"),
+//             trailing: Icon(Icons.chevron_right),
+//           )
