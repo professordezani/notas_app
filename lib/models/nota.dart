d008b537ac691e5ee9556aad66cb3a527d380385
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-10-07 20:14:25 -0300

    Atualiza model Nota com json

--- lib/models/nota.dart
+++ lib/models/nota.dart
@@ -5,4 +5,20 @@ class Nota {
   DateTime date;
 
   Nota(this.id, this.title, this.date);
+
+  factory Nota.fromJson(Map<String, dynamic> json) {
+    return Nota(
+      json['id'] as String,
+      json['title'] as String,
+      DateTime.parse(json['date'] as String)
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "title": title,
+      "date": date.toIso8601String(),
+    };
+  }
 }
